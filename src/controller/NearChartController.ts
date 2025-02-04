f2a80fbce30e343a8c93e76616a4ff311316ca65
ChothaninThitisrirat <maithong31.mmm@gmail.com>
2025-02-05 00:54:13 +0700

    Near Chart function
    - return va and risk of each eye
    - handle out of range

--- src/controller/NearChartController.ts
+++ src/controller/NearChartController.ts
@@ -4,53 +4,76 @@ export const nearchart = async (req: Request,res: Response) => {
     try {
         const { near1, near2, near3, near4 } = req.body
         
-        //Handle missing inputs
+        //Handle missing inputs and inputs out of range 
         if (!near1 || !near2 || !near3 || !near4) {
-            res.status(400).json({
+            res.status(404).json({
                 success: false,
                 message: "Missing required inputs."
             })
             return
+        } else if (near1 <= 0 || near1 >= 12 ||
+            near2 <= 0 || near2 >= 12 ||
+            near3 <= 0 || near3 >= 12 ||
+            near4 <= 0 || near4 >= 12
+        ) {
+            res.status(404).json({
+                success: false,
+                message: "Invaild inputs."
+            })
+            return
         }
 
-        //Estimate line
-        const right = (near1 + near2)/ 2
-        const left = (near3 + near4)/ 2
-        const both_eye = (right + left)/ 2
-
-        //VA
-        
-
-        //Response success
-        if (both_eye <= 3) {
-            res.status(200).send({
-                right,
-                left,
-                both_eye,
-                risk: 3,
-                message: "Send VA successfully"
-            })
-            return
-        } else if (both_eye <= 6){
-            res.status(200).send({
-                right,
-                left,
-                both_eye,
-                risk: 2,
-                message: "Send VA successfully"
-            })
-            return
+        //Right line
+        let right = 0
+        if (near1 > near2) {
+            right = near1
         } else {
-            res.status(200).send({
-                right,
-                left,
-                both_eye,
-                risk: 3,
-                message: "Send VA successfully"
-            })
-            return
+            right = near2
+        }
+        //Left line
+        let left = 0 
+        if (near3 > near4) {
+            left = near3
+        } else {
+            left = near4
+        }
+        //VA
+        const va = ["20/200", "20/100", "20/70", "20/50", "20/40", "20/30", "20/25", "20/20", "20/20", "20/20", "20/20"]
+        const right_va = va[right-1]
+        const left_va = va[left-1]
+
+        //Risk
+        const all_risk = [3, 2, 1]
+        let right_risk = 0
+        let left_risk = 0
+
+        if (right%3 == 0) {
+            right_risk = all_risk[(Math.floor(right/3)) - 1]
+        } else {
+            right_risk = all_risk[Math.floor(right/3)]
         }
 
+        if (left%3 == 0) {
+            left_risk = all_risk[(Math.floor(left/3)) - 1]
+        } else {
+            left_risk = all_risk[Math.floor(left/3)]
+        }
+        
+        //Response success
+        res.status(200).send({
+            right_eye: {
+                right_line: right,
+                right_va,
+                right_risk
+            },
+            left_eye: {
+                left_line: left,
+                left_va,
+                left_risk
+            },
+            success: true,
+            message: "Near chart result sent successfully."
+        })
     } catch (error) {
         //Response Error
         console.log(error);
