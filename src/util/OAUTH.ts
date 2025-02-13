b04881d545694861b4342f52c5174fc0fda9c234
ChothaninThitisrirat <maithong31.mmm@gmail.com>
2025-02-13 14:16:54 +0700

    update google client id
    - add google client id for andriod and ios

--- src/util/OAUTH.ts
+++ src/util/OAUTH.ts
@@ -1,5 +1,13 @@
 import { OAuth2Client } from 'google-auth-library';
 
-export const client = new OAuth2Client(
-    process.env.GOOGLE_CLIENT_ID
-)
+export const google_client = [
+    process.env.GOOGLE_CLIENT_ID_WEB,
+    process.env.GOOGLE_CLIENT_ID_ANDROID,
+    process.env.GOOGLE_CLIENT_ID_IOS    
+].filter((id): id is string => Boolean(id))
+
+if (google_client.length === 0) {
+    throw new Error("Missing Google Client IDs in environment variables.");
+}
+
+export const client = new OAuth2Client(google_client[0]);
